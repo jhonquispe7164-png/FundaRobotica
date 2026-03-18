24b88e95e8f4ea15507be5a64d65c1cebd29cee7
Cristhian Quispe <jhonquispe7164@gmail.com>
2026-03-18 17:06:40 -0500

    Delete lab1 directory

--- lab1/lab1/array_properties.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import numpy as np
-
-#propiedades de una matriz
-if __name__ == "__main__": # Indica que inicia el programa principal
- arreglo = np.array([[1, 2, 3], [4, 5, 6]])
- print(arreglo.shape) # Obtener la forma del arreglo
- print(arreglo.size) # Obtener el número de elementos
- print(arreglo.dtype) # Obtener el tipo de datos
--- lab1/lab1/funcion_sample.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-def saludar(nombre):
- mensaje = "Hola, " + nombre
- return mensaje
-
-#el main
-if __name__ == "__main__": # Indica que inicia el programa principal
- saludo = saludar("Mundo_Cristhian") # Llamado a la funcion
- print(saludo) # Impresion del resultado de la funcion
